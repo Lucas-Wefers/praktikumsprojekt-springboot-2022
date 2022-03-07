9f7182c2817a1ce759d16c75366dd6aacb4f5769
AleGra1 <alex.graf44@gmail.com>
2022-03-07 09:47:33 +0100

    Spring init

---
+++ chicken/src/main/java/de/hhu/chicken/PraktikumGruppe25Application.java
@@ -0,0 +1,13 @@
+package de.hhu.chicken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraktikumGruppe25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PraktikumGruppe25Application.class, args);
+    }
+
+}
---
+++ chicken/src/test/java/de/hhu/chicken/PraktikumGruppe25ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hhu.chicken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraktikumGruppe25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
