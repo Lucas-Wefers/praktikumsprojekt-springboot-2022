e76fa7575b58d7baaa2db575f0c65538f1c97d3b
Noah Tahiri <thrnoah@mbp-von-noah.speedport.ip>
2022-03-07 13:04:58 +0100

    updated build gradle

--- chicken/src/test/java/de/hhu/chicken/architecture/ArchitectureTest.java
+++ chicken/src/test/java/de/hhu/chicken/architecture/ArchitectureTest.java
@@ -1,8 +1,20 @@
 package de.hhu.chicken.architecture;
 
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.library.Architectures.onionArchitecture;
+import static com.tngtech.archunit.library.plantuml.PlantUmlArchCondition.Configurations.consideringOnlyDependenciesInDiagram;
+import static com.tngtech.archunit.library.plantuml.PlantUmlArchCondition.adhereToPlantUmlDiagram;
+
+import com.tngtech.archunit.core.importer.ImportOption;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import de.hhu.chicken.ChickenApplication;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
+@AnalyzeClasses(packagesOf = ChickenApplication.class, importOptions = ImportOption.DoNotIncludeTests.class)
 public class ArchitectureTest {
-
+  
 }
