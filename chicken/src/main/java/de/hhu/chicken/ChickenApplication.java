d51a04b0c4693b120d13fe224583e880110ee864
Lucas-Wefers <luwef100@hhu.de>
2022-03-07 10:25:51 +0100

    Docker compose added

--- chicken/src/main/java/de/hhu/chicken/ChickenApplication.java
+++ chicken/src/main/java/de/hhu/chicken/ChickenApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PraktikumGruppe25Application {
+public class ChickenApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PraktikumGruppe25Application.class, args);
+        SpringApplication.run(ChickenApplication.class, args);
     }
 
 }
