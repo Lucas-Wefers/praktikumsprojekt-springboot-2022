9c4975cbcf31943f50eb590f9bc4227f28722fee
Lucas-Wefers <luwef100@hhu.de>
2022-03-07 12:28:19 +0100

    checkstyle config added

--- chicken/src/main/java/de/hhu/chicken/ChickenApplication.java
+++ chicken/src/main/java/de/hhu/chicken/ChickenApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChickenApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ChickenApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(ChickenApplication.class, args);
+  }
 
 }
