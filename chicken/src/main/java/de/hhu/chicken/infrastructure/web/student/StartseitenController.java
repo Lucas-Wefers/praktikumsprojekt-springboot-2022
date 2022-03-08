2e61e0721e3f4e4fe3e5a4c7eb86f745364e5d92
Lucas-Wefers <luwef100@hhu.de>
2022-03-08 10:54:55 +0100

    added index and mapping for it

--- chicken/src/main/java/de/hhu/chicken/infrastructure/web/student/StartseitenController.java
+++ chicken/src/main/java/de/hhu/chicken/infrastructure/web/student/StartseitenController.java
@@ -1,5 +1,15 @@
 package de.hhu.chicken.infrastructure.web.student;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class StartseitenController {
 
+  @GetMapping("/")
+  @Secured("ROLE_USER")
+  public String index() {
+    return "index";
+  }
 }
