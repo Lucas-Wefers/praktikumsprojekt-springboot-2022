000aa41817fe563dc0d813dbfa5de6c53796b72c
thrnoah <noah.thr6@gmail.com>
2022-03-08 11:31:40 +0100

    added templates and controllers for organisator and tutor

--- chicken/src/main/java/de/hhu/chicken/infrastructure/web/organisator/OrganisatorController.java
+++ chicken/src/main/java/de/hhu/chicken/infrastructure/web/organisator/OrganisatorController.java
@@ -1,5 +1,16 @@
 package de.hhu.chicken.infrastructure.web.organisator;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
 public class OrganisatorController {
 
+  @GetMapping("/organisator")
+  @Secured("ROLE_ADMIN")
+  public String index() {
+    return "organisatorIndex";
+  }
 }
--- chicken/src/main/java/de/hhu/chicken/infrastructure/web/tutor/TutorController.java
+++ chicken/src/main/java/de/hhu/chicken/infrastructure/web/tutor/TutorController.java
@@ -1,5 +1,15 @@
 package de.hhu.chicken.infrastructure.web.tutor;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TutorController {
 
+  @GetMapping("/tutor")
+  @Secured("ROLE_LEADER")
+  public String index() {
+    return "tutorIndex";
+  }
 }
