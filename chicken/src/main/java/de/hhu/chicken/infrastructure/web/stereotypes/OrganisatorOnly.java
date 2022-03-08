dd0c1fdf0bd4efd271f9f06c007e322893fda7c5
Lucas-Wefers <luwef100@hhu.de>
2022-03-08 12:49:35 +0100

    added security annotations for roles

---
+++ chicken/src/main/java/de/hhu/chicken/infrastructure/web/stereotypes/OrganisatorOnly.java
@@ -0,0 +1,14 @@
+package de.hhu.chicken.infrastructure.web.stereotypes;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.security.access.annotation.Secured;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Secured("ROLE_ORGANISATOR")
+public @interface OrganisatorOnly {
+}
---
+++ chicken/src/main/java/de/hhu/chicken/infrastructure/web/stereotypes/TutorOnly.java
@@ -0,0 +1,16 @@
+package de.hhu.chicken.infrastructure.web.stereotypes;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.security.access.annotation.Secured;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Secured("ROLE_TUTOR")
+public @interface TutorOnly {
+
+}
