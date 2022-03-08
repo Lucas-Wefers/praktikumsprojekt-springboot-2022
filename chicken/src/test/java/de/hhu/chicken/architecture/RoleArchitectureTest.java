29a7948b4c6d194d4879921775061dd116b1322b
Lucas-Wefers <luwef100@hhu.de>
2022-03-08 13:09:42 +0100

    added archtests for controller role annotations

---
+++ chicken/src/test/java/de/hhu/chicken/architecture/RoleArchitectureTest.java
@@ -0,0 +1,40 @@
+package de.hhu.chicken.architecture;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import de.hhu.chicken.ChickenApplication;
+import de.hhu.chicken.infrastructure.web.stereotypes.OrganisatorOnly;
+import de.hhu.chicken.infrastructure.web.stereotypes.StudentOnly;
+import de.hhu.chicken.infrastructure.web.stereotypes.TutorOnly;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@AnalyzeClasses(packagesOf = ChickenApplication.class,
+    importOptions = ImportOption.DoNotIncludeTests.class)
+public class RoleArchitectureTest {
+
+  @ArchTest
+  static final ArchRule studentControllersAreAnnotatedWithStudentOnly = classes()
+      .that()
+      .resideInAPackage("..web.student..")
+      .should()
+      .beAnnotatedWith(StudentOnly.class);
+
+  @ArchTest
+  static final ArchRule tutorControllersAreAnnotatedWithTutorOnly = classes()
+      .that()
+      .resideInAPackage("..web.tutor..")
+      .should()
+      .beAnnotatedWith(TutorOnly.class);
+
+  @ArchTest
+  static final ArchRule organisatorControllersAreAnnotatedWithOrganisatorOnly = classes()
+      .that()
+      .resideInAPackage("..web.organisator..")
+      .should()
+      .beAnnotatedWith(OrganisatorOnly.class);
+}
