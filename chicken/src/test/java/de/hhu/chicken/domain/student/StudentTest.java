2e967591b0251aaa2339f2e3366d72a9d3a3b2b3
deyirao <82342038+deyirao@users.noreply.github.com>
2022-03-15 13:40:47 +0100

    refactored tests for fuegeKlausurHinzu in student

--- chicken/src/test/java/de/hhu/chicken/domain/student/StudentTest.java
+++ chicken/src/test/java/de/hhu/chicken/domain/student/StudentTest.java
@@ -9,20 +9,27 @@ import org.junit.jupiter.api.Test;
 
 public class StudentTest {
 
+  private Urlaubstermin urlaubsterminTemplate(int hVon, int minVon, int hBis, int minBis) {
+    return new Urlaubstermin(LocalDate.of(2022, 2, 15),
+        LocalTime.of(hVon, minVon),
+        LocalTime.of(hBis, minBis));
+  }
+
+  private void fuegeUrlaubsterminHinzu(Urlaubstermin urlaubstermin, Student student,
+                                       boolean istKlausurtag) {
+    student.fuegeUrlaubsterminHinzu(urlaubstermin.getDatum(),
+        urlaubstermin.getVon(),
+        urlaubstermin.getBis(),
+        istKlausurtag);
+  }
+
   @Test
   @DisplayName("Ein Urlaubsblock von 4 Stunden wird hinzugefuegt")
   void test_1() {
-    Urlaubstermin urlaubstermin = new Urlaubstermin(
-        LocalDate.of(2022, 2, 15),
-        LocalTime.of(9, 30),
-        LocalTime.of(13, 30));
+    Urlaubstermin urlaubstermin = urlaubsterminTemplate(9, 30, 13, 30);
     Student student = new Student("Jens");
 
-    student.fuegeUrlaubsterminHinzu(
-        urlaubstermin.getDatum(),
-        urlaubstermin.getVon(),
-        urlaubstermin.getBis(),
-        false);
+    fuegeUrlaubsterminHinzu(urlaubstermin, student, false);
 
     assertThat(student.getUrlaubstermine()).hasSize(1);
     assertThat(student.getUrlaubstermine().get(0)).isEqualTo(urlaubstermin);
@@ -31,18 +38,11 @@ public class StudentTest {
   @Test
   @DisplayName("Ein Urlaubsblock von 2 Stunden wird hinzugefuegt")
   void test_2() {
-    Urlaubstermin urlaubstermin = new Urlaubstermin(
-        LocalDate.of(2022, 2, 15),
-        LocalTime.of(9, 30),
-        LocalTime.of(11, 30));
+    Urlaubstermin urlaubstermin = urlaubsterminTemplate(9, 30, 11, 30);
 
     Student student = new Student("Jens");
 
-    student.fuegeUrlaubsterminHinzu(
-        urlaubstermin.getDatum(),
-        urlaubstermin.getVon(),
-        urlaubstermin.getBis(),
-        false);
+    fuegeUrlaubsterminHinzu(urlaubstermin, student, false);
 
     assertThat(student.getUrlaubstermine()).hasSize(1);
     assertThat(student.getUrlaubstermine().get(0)).isEqualTo(urlaubstermin);
@@ -51,18 +51,11 @@ public class StudentTest {
   @Test
   @DisplayName("Ein Urlaubsblock von 3 Stunden wird nicht hinzugefuegt")
   void test_3() {
-    Urlaubstermin urlaubstermin = new Urlaubstermin(
-        LocalDate.of(2022, 2, 15),
-        LocalTime.of(10, 30),
-        LocalTime.of(13, 30));
+    Urlaubstermin urlaubstermin = urlaubsterminTemplate(10, 30, 13, 30);
 
     Student student = new Student("Jens");
 
-    student.fuegeUrlaubsterminHinzu(
-        urlaubstermin.getDatum(),
-        urlaubstermin.getVon(),
-        urlaubstermin.getBis(),
-        false);
+    fuegeUrlaubsterminHinzu(urlaubstermin, student, false);
 
     assertThat(student.getUrlaubstermine()).hasSize(0);
   }
@@ -71,26 +64,12 @@ public class StudentTest {
   @DisplayName("Zwei Urlaubstermine am Anfang und Ende, die je eine Stunde dauern, werden "
       + "hinzugefuegt")
   void test_4() {
-    Urlaubstermin urlaubstermin = new Urlaubstermin(
-        LocalDate.of(2022, 2, 15),
-        LocalTime.of(9, 30),
-        LocalTime.of(10, 30));
-    Urlaubstermin urlaubstermin2 = new Urlaubstermin(
-        LocalDate.of(2022, 2, 15),
-        LocalTime.of(12, 30),
-        LocalTime.of(13, 30));
+    Urlaubstermin urlaubstermin = urlaubsterminTemplate(9, 30, 10, 30);
+    Urlaubstermin urlaubstermin2 = urlaubsterminTemplate(12, 30, 13, 30);
     Student student = new Student("Jens");
 
-    student.fuegeUrlaubsterminHinzu(
-        urlaubstermin.getDatum(),
-        urlaubstermin.getVon(),
-        urlaubstermin.getBis(),
-        false);
-    student.fuegeUrlaubsterminHinzu(
-        urlaubstermin2.getDatum(),
-        urlaubstermin2.getVon(),
-        urlaubstermin2.getBis(),
-        false);
+    fuegeUrlaubsterminHinzu(urlaubstermin, student, false);
+    fuegeUrlaubsterminHinzu(urlaubstermin2, student, false);
 
     assertThat(student.getUrlaubstermine()).hasSize(2);
     assertThat(student.getUrlaubstermine().get(0)).isEqualTo(urlaubstermin);
@@ -101,26 +80,12 @@ public class StudentTest {
   @DisplayName("Zwei Urlaubstermine am Ende und Anfang (in dieser Reihenfolge), "
       + "die je eine Stunde dauern, werden hinzugefuegt")
   void test_5() {
-    Urlaubstermin urlaubstermin2 = new Urlaubstermin(
-        LocalDate.of(2022, 2, 15),
-        LocalTime.of(9, 30),
-        LocalTime.of(10, 30));
-    Urlaubstermin urlaubstermin = new Urlaubstermin(
-        LocalDate.of(2022, 2, 15),
-        LocalTime.of(12, 30),
-        LocalTime.of(13, 30));
+    Urlaubstermin urlaubstermin2 = urlaubsterminTemplate(9, 30, 10, 30);
+    Urlaubstermin urlaubstermin = urlaubsterminTemplate(12, 30, 13, 30);
     Student student = new Student("Jens");
 
-    student.fuegeUrlaubsterminHinzu(
-        urlaubstermin.getDatum(),
-        urlaubstermin.getVon(),
-        urlaubstermin.getBis(),
-        false);
-    student.fuegeUrlaubsterminHinzu(
-        urlaubstermin2.getDatum(),
-        urlaubstermin2.getVon(),
-        urlaubstermin2.getBis(),
-        false);
+    fuegeUrlaubsterminHinzu(urlaubstermin, student, false);
+    fuegeUrlaubsterminHinzu(urlaubstermin2, student, false);
 
     assertThat(student.getUrlaubstermine()).hasSize(2);
     assertThat(student.getUrlaubstermine().get(0)).isEqualTo(urlaubstermin);
@@ -130,26 +95,12 @@ public class StudentTest {
   @Test
   @DisplayName("Am Anfang 2h Urlaubsblock, am Ende 1h Urlaubsblock, wird nur der 1. hinzugefuegt")
   void test_6() {
-    Urlaubstermin urlaubstermin = new Urlaubstermin(
-        LocalDate.of(2022, 2, 15),
-        LocalTime.of(9, 30),
-        LocalTime.of(11, 30));
-    Urlaubstermin urlaubstermin2 = new Urlaubstermin(
-        LocalDate.of(2022, 2, 15),
-        LocalTime.of(12, 30),
-        LocalTime.of(13, 30));
+    Urlaubstermin urlaubstermin = urlaubsterminTemplate(9, 30, 11, 30);
+    Urlaubstermin urlaubstermin2 = urlaubsterminTemplate(12, 30, 13, 30);
     Student student = new Student("Jens");
 
-    student.fuegeUrlaubsterminHinzu(
-        urlaubstermin.getDatum(),
-        urlaubstermin.getVon(),
-        urlaubstermin.getBis(),
-        false);
-    student.fuegeUrlaubsterminHinzu(
-        urlaubstermin2.getDatum(),
-        urlaubstermin2.getVon(),
-        urlaubstermin2.getBis(),
-        false);
+    fuegeUrlaubsterminHinzu(urlaubstermin, student, false);
+    fuegeUrlaubsterminHinzu(urlaubstermin2, student, false);
 
     assertThat(student.getUrlaubstermine()).hasSize(1);
     assertThat(student.getUrlaubstermine().get(0)).isEqualTo(urlaubstermin);
@@ -158,26 +109,12 @@ public class StudentTest {
   @Test
   @DisplayName("Am Anfang 1h Urlaubsblock, am Ende 2h Urlaubsblock, wird nur der 1. hinzugefuegt")
   void test_7() {
-    Urlaubstermin urlaubstermin = new Urlaubstermin(
-        LocalDate.of(2022, 2, 15),
-        LocalTime.of(9, 30),
-        LocalTime.of(10, 30));
-    Urlaubstermin urlaubstermin2 = new Urlaubstermin(
-        LocalDate.of(2022, 2, 15),
-        LocalTime.of(11, 30),
-        LocalTime.of(13, 30));
+    Urlaubstermin urlaubstermin = urlaubsterminTemplate(9, 30, 10, 30);
+    Urlaubstermin urlaubstermin2 = urlaubsterminTemplate(11, 30, 13, 30);
     Student student = new Student("Jens");
 
-    student.fuegeUrlaubsterminHinzu(
-        urlaubstermin.getDatum(),
-        urlaubstermin.getVon(),
-        urlaubstermin.getBis(),
-        false);
-    student.fuegeUrlaubsterminHinzu(
-        urlaubstermin2.getDatum(),
-        urlaubstermin2.getVon(),
-        urlaubstermin2.getBis(),
-        false);
+    fuegeUrlaubsterminHinzu(urlaubstermin, student, false);
+    fuegeUrlaubsterminHinzu(urlaubstermin2, student, false);
 
     assertThat(student.getUrlaubstermine()).hasSize(1);
     assertThat(student.getUrlaubstermine().get(0)).isEqualTo(urlaubstermin);
@@ -186,26 +123,12 @@ public class StudentTest {
   @Test
   @DisplayName("Am Anfang 1h Urlaubsblock, mittendrin 1h Urlaubsblock, wird nur der 1. hinzugefuegt")
   void test_8() {
-    Urlaubstermin urlaubstermin = new Urlaubstermin(
-        LocalDate.of(2022, 2, 15),
-        LocalTime.of(9, 30),
-        LocalTime.of(10, 30));
-    Urlaubstermin urlaubstermin2 = new Urlaubstermin(
-        LocalDate.of(2022, 2, 15),
-        LocalTime.of(11, 30),
-        LocalTime.of(12, 30));
+    Urlaubstermin urlaubstermin = urlaubsterminTemplate(9, 30, 10, 30);
+    Urlaubstermin urlaubstermin2 = urlaubsterminTemplate(11, 30, 12, 30);
     Student student = new Student("Jens");
 
-    student.fuegeUrlaubsterminHinzu(
-        urlaubstermin.getDatum(),
-        urlaubstermin.getVon(),
-        urlaubstermin.getBis(),
-        false);
-    student.fuegeUrlaubsterminHinzu(
-        urlaubstermin2.getDatum(),
-        urlaubstermin2.getVon(),
-        urlaubstermin2.getBis(),
-        false);
+    fuegeUrlaubsterminHinzu(urlaubstermin, student, false);
+    fuegeUrlaubsterminHinzu(urlaubstermin2, student, false);
 
     assertThat(student.getUrlaubstermine()).hasSize(1);
     assertThat(student.getUrlaubstermine().get(0)).isEqualTo(urlaubstermin);
@@ -214,26 +137,12 @@ public class StudentTest {
   @Test
   @DisplayName("Mittendrin 1h Urlaubsblock, am Ende 2h Urlaubsblock, wird nur der 1. hinzugefuegt")
   void test_9() {
-    Urlaubstermin urlaubstermin = new Urlaubstermin(
-        LocalDate.of(2022, 2, 15),
-        LocalTime.of(10, 30),
-        LocalTime.of(11, 30));
-    Urlaubstermin urlaubstermin2 = new Urlaubstermin(
-        LocalDate.of(2022, 2, 15),
-        LocalTime.of(11, 30),
-        LocalTime.of(13, 30));
+    Urlaubstermin urlaubstermin = urlaubsterminTemplate(10, 30, 11, 30);
+    Urlaubstermin urlaubstermin2 = urlaubsterminTemplate(11, 30, 13, 30);
     Student student = new Student("Jens");
 
-    student.fuegeUrlaubsterminHinzu(
-        urlaubstermin.getDatum(),
-        urlaubstermin.getVon(),
-        urlaubstermin.getBis(),
-        false);
-    student.fuegeUrlaubsterminHinzu(
-        urlaubstermin2.getDatum(),
-        urlaubstermin2.getVon(),
-        urlaubstermin2.getBis(),
-        false);
+    fuegeUrlaubsterminHinzu(urlaubstermin, student, false);
+    fuegeUrlaubsterminHinzu(urlaubstermin2, student, false);
 
     assertThat(student.getUrlaubstermine()).hasSize(1);
     assertThat(student.getUrlaubstermine().get(0)).isEqualTo(urlaubstermin);
@@ -242,26 +151,12 @@ public class StudentTest {
   @Test
   @DisplayName("Bei zwei 1h Urlaubsbloecken mittendrin, wird nur der 1. hinzugefuegt")
   void test_10() {
-    Urlaubstermin urlaubstermin = new Urlaubstermin(
-        LocalDate.of(2022, 2, 15),
-        LocalTime.of(10, 30),
-        LocalTime.of(11, 30));
-    Urlaubstermin urlaubstermin2 = new Urlaubstermin(
-        LocalDate.of(2022, 2, 15),
-        LocalTime.of(12, 0),
-        LocalTime.of(13, 0));
+    Urlaubstermin urlaubstermin = urlaubsterminTemplate(10, 30, 11, 30);
+    Urlaubstermin urlaubstermin2 = urlaubsterminTemplate(12, 0, 13, 0);
     Student student = new Student("Jens");
 
-    student.fuegeUrlaubsterminHinzu(
-        urlaubstermin.getDatum(),
-        urlaubstermin.getVon(),
-        urlaubstermin.getBis(),
-        false);
-    student.fuegeUrlaubsterminHinzu(
-        urlaubstermin2.getDatum(),
-        urlaubstermin2.getVon(),
-        urlaubstermin2.getBis(),
-        false);
+    fuegeUrlaubsterminHinzu(urlaubstermin, student, false);
+    fuegeUrlaubsterminHinzu(urlaubstermin2, student, false);
 
     assertThat(student.getUrlaubstermine()).hasSize(1);
     assertThat(student.getUrlaubstermine().get(0)).isEqualTo(urlaubstermin);
@@ -271,35 +166,14 @@ public class StudentTest {
   @DisplayName("Bei drei 30min Urlaubsbloecken am Anfang und Ende und einem mittendrin, werden nur"
       + " 2 hinzugefuegt")
   void test_11() {
-    Urlaubstermin urlaubstermin = new Urlaubstermin(
-        LocalDate.of(2022, 2, 15),
-        LocalTime.of(9, 30),
-        LocalTime.of(10, 0));
-    Urlaubstermin urlaubstermin2 = new Urlaubstermin(
-        LocalDate.of(2022, 2, 15),
-        LocalTime.of(13, 0),
-        LocalTime.of(13, 30));
-    Urlaubstermin urlaubstermin3 = new Urlaubstermin(
-        LocalDate.of(2022, 2, 15),
-        LocalTime.of(11, 0),
-        LocalTime.of(11, 30));
+    Urlaubstermin urlaubstermin = urlaubsterminTemplate(9, 30, 10, 0);
+    Urlaubstermin urlaubstermin2 = urlaubsterminTemplate(13, 0, 13, 30);
+    Urlaubstermin urlaubstermin3 = urlaubsterminTemplate(11, 0, 11, 30);
     Student student = new Student("Jens");
 
-    student.fuegeUrlaubsterminHinzu(
-        urlaubstermin.getDatum(),
-        urlaubstermin.getVon(),
-        urlaubstermin.getBis(),
-        false);
-    student.fuegeUrlaubsterminHinzu(
-        urlaubstermin2.getDatum(),
-        urlaubstermin2.getVon(),
-        urlaubstermin2.getBis(),
-        false);
-    student.fuegeUrlaubsterminHinzu(
-        urlaubstermin3.getDatum(),
-        urlaubstermin3.getVon(),
-        urlaubstermin3.getBis(),
-        false);
+    fuegeUrlaubsterminHinzu(urlaubstermin, student, false);
+    fuegeUrlaubsterminHinzu(urlaubstermin2, student, false);
+    fuegeUrlaubsterminHinzu(urlaubstermin3, student, false);
 
     assertThat(student.getUrlaubstermine()).hasSize(2);
     assertThat(student.getUrlaubstermine().get(0)).isEqualTo(urlaubstermin);
@@ -310,35 +184,14 @@ public class StudentTest {
   @DisplayName("Bei drei 30min Urlaubsbloecken mittendrin, werden an einem Klausurtag alle " +
       "hinzugefuegt")
   void test_12() {
-    Urlaubstermin urlaubstermin = new Urlaubstermin(
-        LocalDate.of(2022, 2, 15),
-        LocalTime.of(10, 30),
-        LocalTime.of(11, 0));
-    Urlaubstermin urlaubstermin2 = new Urlaubstermin(
-        LocalDate.of(2022, 2, 15),
-        LocalTime.of(12, 0),
-        LocalTime.of(12, 30));
-    Urlaubstermin urlaubstermin3 = new Urlaubstermin(
-        LocalDate.of(2022, 2, 15),
-        LocalTime.of(9, 0),
-        LocalTime.of(9, 30));
+    Urlaubstermin urlaubstermin = urlaubsterminTemplate(10, 30, 11, 0);
+    Urlaubstermin urlaubstermin2 = urlaubsterminTemplate(12, 0, 12, 30);
+    Urlaubstermin urlaubstermin3 = urlaubsterminTemplate(9, 0, 9, 30);
     Student student = new Student("Jens");
 
-    student.fuegeUrlaubsterminHinzu(
-        urlaubstermin.getDatum(),
-        urlaubstermin.getVon(),
-        urlaubstermin.getBis(),
-        true);
-    student.fuegeUrlaubsterminHinzu(
-        urlaubstermin2.getDatum(),
-        urlaubstermin2.getVon(),
-        urlaubstermin2.getBis(),
-        true);
-    student.fuegeUrlaubsterminHinzu(
-        urlaubstermin3.getDatum(),
-        urlaubstermin3.getVon(),
-        urlaubstermin3.getBis(),
-        true);
+    fuegeUrlaubsterminHinzu(urlaubstermin, student, true);
+    fuegeUrlaubsterminHinzu(urlaubstermin2, student, true);
+    fuegeUrlaubsterminHinzu(urlaubstermin3, student, true);
 
     assertThat(student.getUrlaubstermine()).hasSize(3);
     assertThat(student.getUrlaubstermine().get(0)).isEqualTo(urlaubstermin);
