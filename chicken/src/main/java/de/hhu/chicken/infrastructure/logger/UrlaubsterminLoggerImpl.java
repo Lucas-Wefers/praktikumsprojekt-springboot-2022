0246271effc8b1afcb5728100a3f948a7f411c8d
Lucas-Wefers <luwef100@hhu.de>
2022-03-23 11:42:49 +0100

    implemented urlaubsterminLoggerImpl

--- chicken/src/main/java/de/hhu/chicken/infrastructure/logger/UrlaubsterminLoggerImpl.java
+++ chicken/src/main/java/de/hhu/chicken/infrastructure/logger/UrlaubsterminLoggerImpl.java
@@ -1,13 +1,38 @@
 package de.hhu.chicken.infrastructure.logger;
 
 import de.hhu.chicken.service.logger.UrlaubsterminLogger;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UrlaubsterminLoggerImpl implements UrlaubsterminLogger {
 
+  private final File logFile;
+
+  public UrlaubsterminLoggerImpl() throws IOException {
+    logFile = new File("urlaubslog.txt");
+    logFile.createNewFile();
+  }
+
   @Override
   public void eintragen(String logNachricht) {
-
+    try {
+      BufferedWriter writer = new BufferedWriter(new FileWriter(logFile, true));
+      LocalDateTime currentTime = LocalDateTime.now();
+      DateTimeFormatter uhrzeitFormat = DateTimeFormatter.ofPattern("HH:mm:ss");
+      String logDatum = currentTime.format(DateTimeFormatter.ISO_LOCAL_DATE);
+      String logZeit = currentTime.format(uhrzeitFormat);
+      writer.write("[" + logDatum + "] [" + logZeit + "] " + logNachricht);
+      writer.newLine();
+      writer.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
